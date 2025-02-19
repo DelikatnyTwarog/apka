8a2dd2623114bd1640b3c91c6d33035e13650ebe
DelikatnyTwarog <useba@o2.pl>
2025-02-19 00:41:10 +0000

    FAJNY UPDATE

--- app/src/main/java/com/example/apka/SecondFragment.kt
+++ app/src/main/java/com/example/apka/SecondFragment.kt
@@ -32,7 +32,7 @@ class SecondFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-        //println(dupa)
+        println(dupa)
 
         //MOJE KODY SOM ZAYEBYSTE!!!!
     }
